cdf42277958509dd9642e7c947eb9ba316e24ef9
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-14 17:53:25 +0530

    feat: Button to setup stripe webhook

--- digistore/digistore/doctype/digistore_settings/digistore_settings.js
+++ digistore/digistore/doctype/digistore_settings/digistore_settings.js
@@ -1,8 +1,11 @@
 // Copyright (c) 2021, OS and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('DigiStore Settings', {
-	// refresh: function(frm) {
+frappe.ui.form.on("DigiStore Settings", {
+  create_stripe_webhook(frm) {
+    frm.call("create_stripe_webhook");
+  },
+  // refresh: function(frm) {
 
-	// }
+  // }
 });
--- digistore/digistore/doctype/digistore_settings/digistore_settings.py
+++ digistore/digistore/doctype/digistore_settings/digistore_settings.py
@@ -1,8 +1,30 @@
 # Copyright (c) 2021, OS and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from digistore.utils.payments import get_stripe
+
 
 class DigiStoreSettings(Document):
-	pass
+	@frappe.whitelist()
+	def create_stripe_webhook(self):
+		stripe = get_stripe()
+		url = frappe.utils.get_url("/api/method/digistore.api.billing.handle_stripe_webhook")
+		webhook = stripe.WebhookEndpoint.create(
+			url=url,
+			enabled_events=[
+				"payment_intent.requires_action",
+				"payment_intent.payment_failed",
+				"payment_intent.succeeded",
+				"payment_method.attached",
+				"invoice.payment_action_required",
+				"invoice.payment_succeeded",
+				"invoice.payment_failed",
+				"invoice.finalized",
+			],
+		)
+		self.stripe_webhook_endpoint_id = webhook["id"]
+		self.stripe_webhook_secret = webhook["secret"]
+		self.flags.ignore_mandatory = True
+		self.save()
