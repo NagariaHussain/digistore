20b43c3d80c3aa83499ce7d04773af1559ee6d3f
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-14 14:08:12 +0530

    feat: Fetch user purchase list

--- store/src/router/auth.js
+++ store/src/router/auth.js
@@ -1,12 +1,19 @@
+import UserProducts from '../views/UserProducts.vue';
+
 export default [
-    {
+	{
 		path: '/login',
 		name: 'Login',
 		component: () =>
 			import(/* webpackChunkName: "login" */ '../views/Login.vue'),
 		meta: {
-			isLoginPage: true
+			isLoginPage: true,
 		},
-		props: true
-	}
-]
+		props: true,
+	},
+	{
+		path: '/purchases',
+		name: 'Purchases',
+		component: UserProducts,
+	},
+];
---
+++ store/src/views/UserProducts.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<h2>Your Products</h2>
+		<ol v-if="$resources.userProducts.data">
+			<li v-for="p in $resources.userProducts.data" :key="p.product.name">
+				{{ p.product.title }}
+			</li>
+		</ol>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UserProducts',
+	resources: {
+		userProducts() {
+			console.log(this.$account.user.name);
+			return {
+				method: 'digistore.api.product.purchased',
+				params: {
+					user: this.$account.user.name,
+				},
+				auto: true,
+				onSuccess(d) {
+					console.log(d);
+				},
+			};
+		},
+	},
+	inject: ['$auth', '$account'],
+};
+</script>
