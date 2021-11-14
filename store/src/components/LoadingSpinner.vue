d8c8ab152790244fd38a3cfd49eb2825b63a8999
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-14 15:47:41 +0530

    refactor: Extract loading spinner component

---
+++ store/src/components/LoadingSpinner.vue
@@ -0,0 +1,57 @@
+<template>
+	<div class="py-5 px-5" v-if="loading">
+		<button
+			type="button"
+			class="
+				inline-flex
+				items-center
+				bg-indigo-500
+				px-4
+				py-2
+				border border-transparent
+				text-base
+				leading-6
+				font-medium
+				rounded-md
+				text-white
+				bg-rose-600
+				hover:bg-rose-500
+				focus:border-rose-700
+				active:bg-rose-700
+				transition
+				ease-in-out
+				duration-150
+				cursor-not-allowed
+			"
+		>
+			<svg
+				class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+			>
+				<circle
+					class="opacity-25"
+					cx="12"
+					cy="12"
+					r="10"
+					stroke="currentColor"
+					stroke-width="4"
+				></circle>
+				<path
+					class="opacity-75"
+					fill="currentColor"
+					d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+				></path>
+			</svg>
+			Loading
+		</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'LoadingSpinner',
+	props: ['loading'],
+};
+</script>
--- store/src/views/ProductsList.vue
+++ store/src/views/ProductsList.vue
@@ -22,53 +22,8 @@
 			</div>
 		</div>
 
-		<div class="py-5 px-5" v-if="$resources.allProducts.loading">
-			<button
-				type="button"
-				class="
-					inline-flex
-					items-center bg-indigo-500
-					px-4
-					py-2
-					border border-transparent
-					text-base
-					leading-6
-					font-medium
-					rounded-md
-					text-white
-					bg-rose-600
-					hover:bg-rose-500
-					focus:border-rose-700
-					active:bg-rose-700
-					transition
-					ease-in-out
-					duration-150
-					cursor-not-allowed
-				"
-			>
-				<svg
-					class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-				>
-					<circle
-						class="opacity-25"
-						cx="12"
-						cy="12"
-						r="10"
-						stroke="currentColor"
-						stroke-width="4"
-					></circle>
-					<path
-						class="opacity-75"
-						fill="currentColor"
-						d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-					></path>
-				</svg>
-				Loading
-			</button>
-		</div>
+		<LoadingSpinner :loading="$resources.allProducts.loading" />
+
 		<div
 			v-if="$resources.allProducts.data"
 			class="
@@ -96,14 +51,16 @@
 								{{ product.category }}
 							</a>
 						</p>
-						<a class="block mt-2">
-							<p class="text-xl font-semibold text-gray-900">
-								{{ product.title }}
-							</p>
-							<p class="mt-3 text-base text-gray-500">
-								{{ product.short_description }}
-							</p>
-						</a>
+						<router-link :to="`/products/${product.name}`" v-slot="{ href }">
+							<a class="block mt-2" :href="href">
+								<p class="text-xl font-semibold text-gray-900">
+									{{ product.title }}
+								</p>
+								<p class="mt-3 text-base text-gray-500">
+									{{ product.short_description }}
+								</p>
+							</a>
+						</router-link>
 					</div>
 				</div>
 			</div>
@@ -112,9 +69,14 @@
 </template>
 
 <script>
+import LoadingSpinner from '../components/LoadingSpinner.vue';
+
 export default {
 	data() {
 		return {};
+	},
+	components: {
+		LoadingSpinner,
 	},
 	resources: {
 		allProducts() {
