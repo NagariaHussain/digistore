214f91d73d82b1fe5e5fb079d89139de2aaf47e8
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-14 16:03:21 +0530

    feat: Add tailwind typography plugin

--- store/tailwind.config.js
+++ store/tailwind.config.js
@@ -1,17 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
