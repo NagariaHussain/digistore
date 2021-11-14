4dcedd166106d92c4129979968237dc410f19f36
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-11-14 12:22:55 +0530

    feat: Base doctypes

---
+++ digistore/digistore/doctype/plan/plan.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, OS and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Plan', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ digistore/digistore/doctype/plan/test_plan.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, OS and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPlan(unittest.TestCase):
+	pass
