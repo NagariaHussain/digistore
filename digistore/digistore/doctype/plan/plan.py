a99a2599529201c8f971d14a512fc4fa6b099ab7
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-04 22:56:47 +0530

    chore: Validation for duplicate asset in assets
    ... child table.

--- digistore/digistore/doctype/plan/plan.py
+++ digistore/digistore/doctype/plan/plan.py
@@ -1,8 +1,14 @@
-# Copyright (c) 2021, OS and contributors
+# Copyright (c) 2021, Mohammad Hussain Nagaria and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
+
 from frappe.model.document import Document
 
+
 class Plan(Document):
-	pass
+	def validate(self):
+		set_of_assets = set(a.asset for a in self.assets)
+
+		if len(set_of_assets) != len(self.assets):
+			frappe.throw("Assets table contains duplicate entries")
