2f6f223a1766e9a21fbe61d1e69dfe9952e8f6ec
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-04 22:37:31 +0530

    fix: Data unloaded

--- store/src/views/Product.vue
+++ store/src/views/Product.vue
@@ -1,84 +1,92 @@
 <template>
 	<div class="mx-auto max-w-7xl">
 		<LoadingSpinner :loading="$resources.productData.loading" />
-		<div class="mt-8 px-4 prose" v-if="productData">
-			<h1>
-				{{ productData.product_doc.title }}
-			</h1>
-			<p>{{ productData.product_doc.short_description }}</p>
-		</div>
-		<h2 class="mt-4 px-4 font-bold text-xl">Plans</h2>
-		<div class="max-w-7xl pb-24 sm:px-4">
-			<div
-				class="
-					mt-3
-					space-y-4
-					sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6
-					lg:max-w-4xl lg:mx-auto
-					xl:max-w-none xl:mx-0 xl:grid-cols-4
-				"
-			>
+		<div v-if="$resources.productData.data">
+			<div class="mt-8 px-4 prose">
+				<h1>
+					{{ productData.product_doc.title }}
+				</h1>
+				<p>{{ productData.product_doc.short_description }}</p>
+			</div>
+			<h2 class="mt-4 px-4 font-bold text-xl">Plans</h2>
+			<div class="max-w-7xl pb-24 sm:px-4">
 				<div
-					v-for="tier in productData.product_plans"
-					:key="tier.title"
 					class="
-						border border-gray-200
-						rounded-lg
-						shadow-sm
-						divide-y divide-gray-200
+						mt-3
+						space-y-4
+						sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-6
+						lg:max-w-4xl lg:mx-auto
+						xl:max-w-none xl:mx-0 xl:grid-cols-4
 					"
 				>
-					<div class="p-6">
-						<h2 class="text-lg leading-6 font-medium text-gray-900">
-							{{ tier.title }}
-						</h2>
-						<p class="mt-4 text-sm text-gray-500">{{ tier.description }}</p>
-						<p class="mt-8">
-							<span class="text-4xl font-extrabold text-gray-900"
-								>${{ tier.price }}</span
-							>
-						</p>
+					<div
+						v-for="tier in productData.product_plans"
+						:key="tier.title"
+						class="
+							border border-gray-200
+							rounded-lg
+							shadow-sm
+							divide-y divide-gray-200
+						"
+					>
+						<div class="p-6">
+							<h2 class="text-lg leading-6 font-medium text-gray-900">
+								{{ tier.title }}
+							</h2>
+							<p class="mt-4 text-sm text-gray-500">{{ tier.description }}</p>
+							<p class="mt-8">
+								<span class="text-4xl font-extrabold text-gray-900"
+									>${{ tier.price }}</span
+								>
+							</p>
 
-						<LoadingSpinner :loading="$resources.buyProduct.loading" />
-						<button
-							v-if="!$resources.buyProduct.loading"
-							@click="$resources.buyProduct.submit({ plan: tier.name })"
-							class="
-								mt-8
-								block
-								w-full
-								bg-gray-800
-								border border-gray-800
-								rounded-md
-								py-2
-								text-sm
-								font-semibold
-								text-white text-center
-								hover:bg-gray-900
-							"
-						>
-							Buy {{ tier.title }}
-						</button>
-					</div>
-					<div class="pt-6 pb-8 px-6">
-						<h3
-							class="text-xs font-medium text-gray-900 tracking-wide uppercase"
-						>
-							What's included
-						</h3>
-						<ul role="list" class="mt-6 space-y-4">
-							<li
-								v-for="feature in ['Feature one', 'Feature 2']"
-								:key="feature"
-								class="flex space-x-3"
+							<LoadingSpinner :loading="$resources.buyProduct.loading" />
+							<button
+								v-if="!$resources.buyProduct.loading"
+								@click="$resources.buyProduct.submit({ plan: tier.name })"
+								class="
+									mt-8
+									block
+									w-full
+									bg-gray-800
+									border border-gray-800
+									rounded-md
+									py-2
+									text-sm
+									font-semibold
+									text-white text-center
+									hover:bg-gray-900
+								"
 							>
-								<CheckIcon
-									class="flex-shrink-0 h-5 w-5 text-green-500"
-									aria-hidden="true"
-								/>
-								<span class="text-sm text-gray-500">{{ feature }}</span>
-							</li>
-						</ul>
+								Buy {{ tier.title }}
+							</button>
+						</div>
+						<div class="pt-6 pb-8 px-6">
+							<h3
+								class="
+									text-xs
+									font-medium
+									text-gray-900
+									tracking-wide
+									uppercase
+								"
+							>
+								What's included
+							</h3>
+							<ul role="list" class="mt-6 space-y-4">
+								<li
+									v-for="feature in ['Feature one', 'Feature 2']"
+									:key="feature"
+									class="flex space-x-3"
+								>
+									<CheckIcon
+										class="flex-shrink-0 h-5 w-5 text-green-500"
+										aria-hidden="true"
+									/>
+									<span class="text-sm text-gray-500">{{ feature }}</span>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -102,9 +110,6 @@ export default {
 					name: this.productName,
 				},
 				auto: true,
-				onSuccess(d) {
-					console.log(d);
-				},
 			};
 		},
 		buyProduct() {
