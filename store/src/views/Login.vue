625edfa2156438a415298d7dc86179d6140194ff
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-05 12:32:50 +0530

    fix: Title on login screen

--- store/src/views/Login.vue
+++ store/src/views/Login.vue
@@ -1,5 +1,9 @@
 <template>
 	<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+		<h2 class="my-6 text-center text-3xl font-extrabold text-gray-900">
+			Sign in to your account
+		</h2>
+
 		<div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
 			<form class="space-y-6" @submit.prevent="login">
 				<div>
