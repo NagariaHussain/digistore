58bc5788f7eb737e677cfe7cd6d903cc8287b6d2
Hussain Nagaria <hussainbhaitech@gmail.com>
2021-12-05 12:29:18 +0530

    feat: Cool login screen

--- store/src/views/Login.vue
+++ store/src/views/Login.vue
@@ -1,47 +1,137 @@
 <template>
-  <div class="min-h-screen bg-white flex">
-    <div class="mx-auto w-full max-w-sm lg:w-96">
-      <form @submit.prevent="login" class="space-y-6">
-        <label for="email"> Username: </label>
-        <input type="text" v-model="email" />
-        <br />
-        <label for="password"> Password: </label>
-        <input type="password" v-model="password" />
+	<div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+		<div class="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+			<form class="space-y-6" @submit.prevent="login">
+				<div>
+					<label for="email" class="block text-sm font-medium text-gray-700">
+						Email address
+					</label>
+					<div class="mt-1">
+						<input
+							v-model="email"
+							id="email"
+							name="email"
+							:type="email !== 'Administrator' ? 'email' : 'text'"
+							autocomplete="email"
+							required="true"
+							class="
+								appearance-none
+								block
+								w-full
+								px-3
+								py-2
+								border border-gray-300
+								rounded-md
+								shadow-sm
+								placeholder-gray-400
+								focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+								sm:text-sm
+							"
+						/>
+					</div>
+				</div>
 
-        <button
-          class="bg-blue-500 block text-white p-2 hover:bg-blue-700"
-          type="submit"
-        >
-          Sign in
-        </button>
-      </form>
-    </div>
-  </div>
+				<div>
+					<label for="password" class="block text-sm font-medium text-gray-700">
+						Password
+					</label>
+					<div class="mt-1">
+						<input
+							v-model="password"
+							id="password"
+							name="password"
+							type="password"
+							autocomplete="current-password"
+							required=""
+							class="
+								appearance-none
+								block
+								w-full
+								px-3
+								py-2
+								border border-gray-300
+								rounded-md
+								shadow-sm
+								placeholder-gray-400
+								focus:outline-none focus:ring-indigo-500 focus:border-indigo-500
+								sm:text-sm
+							"
+						/>
+					</div>
+				</div>
+
+				<ErrorMessage :error="errorMessage" class="mt-4" />
+
+				<div class="flex items-center justify-between">
+					<div class="text-sm">
+						<a
+							href="#"
+							class="font-medium text-indigo-600 hover:text-indigo-500"
+						>
+							Forgot your password?
+						</a>
+					</div>
+				</div>
+
+				<div>
+					<button
+						type="submit"
+						class="
+							w-full
+							flex
+							justify-center
+							py-2
+							px-4
+							border border-transparent
+							rounded-md
+							shadow-sm
+							text-sm
+							font-medium
+							text-white
+							bg-indigo-600
+							hover:bg-indigo-700
+							focus:outline-none
+							focus:ring-2
+							focus:ring-offset-2
+							focus:ring-indigo-500
+						"
+					>
+						Sign in
+					</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      email: null,
-      password: null,
-    };
-  },
-  inject: ["$auth"],
-  async mounted() {
-    if (this.$route?.query?.route) {
-      this.redirect_route = this.$route.query.route;
-      this.$router.replace({ query: null });
-    }
-  },
-  methods: {
-    async login() {
-      if (this.email && this.password) {
-        let res = await this.$auth.login(this.email, this.password);
-        if (res) {
-          this.$router.push({ name: "Home" });
-        }
-      }
-    },
-  },
+	data() {
+		return {
+			email: null,
+			password: null,
+			errorMessage: null,
+		};
+	},
+	inject: ['$auth'],
+	async mounted() {
+		if (this.$route?.query?.route) {
+			this.redirect_route = this.$route.query.route;
+			this.$router.replace({ query: null });
+		}
+	},
+	methods: {
+		async login() {
+			if (this.email && this.password) {
+				try {
+					let res = await this.$auth.login(this.email, this.password);
+					if (res) {
+						this.$router.push({ name: 'Home' });
+					}
+				} catch (e) {
+					this.errorMessage = e.messages?.join('\n');
+				}
+			}
+		},
+	},
 };
 </script>
